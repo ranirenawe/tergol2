bf95de23057fe65e2e2432c225d73ed94206ce31
ranir <ranir@rani.local>
2026-03-29 10:05:18 +0300

    new 2

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,77 +1,78 @@
 /**
+ * @author ranirenawe
+ * @author fadikazoura
  * This class represents shared data used between threads.
  * It contains an integer array, a boolean array, and flags
  * for synchronization between threads.
  */
 public class SharedData 
 {
-	private int [] array;
-	private boolean [] winArray;
-	private boolean flag;
-	private final int b;
+    private ArrayList<Integer> array;
+    private boolean [] winArray;
+    private boolean flag;
+    private final int b;
 	
 	/**
 	 * Constructs a SharedData object.
 	 * @param array the integer array
 	 * @param b an integer value
 	 */
-	public SharedData(int[] array, int b) {
-		
-		this.array = array;
-		this.b = b;
-	}
+    public SharedData(ArrayList<Integer> array, int b) {
+        this.array = array;
+        this.b = b;
+    }
 
 	/**
 	 * Returns the win array.
 	 * @return boolean array representing wins
 	 */
-	public boolean[] getWinArray() 
-	{
-		return winArray;
-	}
+    public boolean[] getWinArray() 
+    {
+        return winArray;
+    }
 
 	/**
 	 * Sets the win array.
 	 * @param winArray the boolean array to set
 	 */
-	public void setWinArray(boolean [] winArray) 
-	{
-		this.winArray = winArray;
-	}
+    public void setWinArray(boolean [] winArray) 
+    {
+        this.winArray = winArray;
+    }
 
 	/**
 	 * Returns the integer array.
 	 * @return the array
 	 */
-	public int[] getArray() 
-	{
-		return array;
-	}
+    public ArrayList<Integer> getArray() 
+    {
+        return array;
+    }
 
 	/**
 	 * Returns the value of b.
 	 * @return integer b
 	 */
-	public int getB() 
-	{
-		return b;
-	}
+    public int getB() 
+    {
+        return b;
+    }
 
 	/**
 	 * Returns the flag value.
 	 * @return true if flag is set, otherwise false
 	 */
-	public boolean getFlag() 
-	{
-		return flag;
-	}
+    public boolean getFlag() 
+    {
+        return flag;
+    }
 
 	/**
 	 * Sets the flag value.
 	 * @param flag the value to set
 	 */
-	public void setFlag(boolean flag) {
-		this.flag = flag;
-	}
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
 
 }
